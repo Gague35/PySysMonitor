a5cb396c550838c4b352a5138245719f63d8cec9
Gague35 <235574890+Gague35@users.noreply.github.com>
2026-04-08 15:55:02 +0200

    fix: fixed CPU and RAM top for the full app.

--- main.py
+++ main.py
@@ -84,29 +84,38 @@ def get_cpu_temp():
 
 # Processes fuction
 def get_top_proc():
-    processes = []
+    grouped = {}
 
     psutil.cpu_percent(interval=None) 
     
-    for proc in psutil.process_iter(['name', 'cpu_percent', 'memory_info']):
+    for proc in psutil.process_iter(['name', 'cpu_percent', 'memory_full_info']):
         try:
+            if proc.info['memory_full_info'] is None:
+                continue
             cpu = proc.info['cpu_percent']
             name = proc.info['name']
-            ramMB = proc.info['memory_info'].rss // 1024**2
+            ramMB = proc.info['memory_full_info'].uss // 1024**2
 
             if name == "System Idle Process" or name is None:
                 continue
                 
-            processes.append({
-                'name': name, 
-                'cpu': round(cpu / cores, 2),
-                'ram': ramMB
-            })
+            if name in grouped:
+                grouped[name]['cpu'] += cpu
+                grouped[name]['ram'] += ramMB
+            else:
+                grouped[name] = {'name': name, 'cpu': cpu, 'ram': ramMB}
+
         except (psutil.NoSuchProcess, psutil.AccessDenied):
             continue
+    
+    for name in grouped:
+        grouped[name]['cpu'] = round(grouped[name]['cpu'] / cores, 2)
 
-    top3_cpu = sorted(processes, key=lambda x: x['cpu'], reverse=True)[:3]
-    top3_ram = sorted(processes, key=lambda x: x['ram'], reverse=True)[:3]
+    grp_proc = grouped.values()
+    proc_list = list(grp_proc)
+
+    top3_cpu = sorted(proc_list, key=lambda x: x['cpu'], reverse=True)[:3]
+    top3_ram = sorted(proc_list, key=lambda x: x['ram'], reverse=True)[:3]
     return top3_cpu, top3_ram
 
 # Main function
